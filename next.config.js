2b6546b3cbfc260302cd6aa451932574e723c157
dmjcomdem <dmjcomdem.style@gmail.com>
2023-01-21 09:15:13 +0500

    feat: Оптимизация данных для PWA

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@ const withPWA = require('next-pwa')({
     dest: 'public',
     register: true,
     skipWaiting: true,
-    pwa:{
-        dest:"public",
-        register:true,
-        skipWaiting:true,
-        disable:process.env.NODE_ENV === 'development'
-    }
 })
 
 module.exports = withPWA({
