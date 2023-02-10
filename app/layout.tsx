fb1ba1fc1b37b0cf726b11e61e3fbeef5c067757
dmjcomdem <dmjcomdem.style@gmail.com>
2023-02-10 06:35:05 +0500

    fix: Добавление user-scalable

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function MainLayout({ children }: { children: ReactNode }) {
         <html lang="ru" translate="no">
             <head>
                 <meta charSet="UTF-8" />
-                <meta name="viewport" content="width=device-width; initial-scale=1; viewport-fit=cover; user-scalable=no" />
+                <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover, user-scalable=no" />
 
                 <title>100 вопросов для вашего душевного общения</title>
                 <meta name="description" content="Приложение для того, чтобы вам было интересно открываться и узнавать друг друга" />
@@ -39,14 +39,13 @@ export default function MainLayout({ children }: { children: ReactNode }) {
                 <link rel="icon" type="image/svg+xml" href="./favicon/any.svg" />
                 <link rel="shortcut icon" href="./favicon/favicon.ico" />
 
-                {/*<meta name="theme-color" content="#5D4BE1" />*/}
-
                 <meta name="mobile-web-app-capable" content="yes" />
                 <meta name="apple-touch-fullscreen" content="yes" />
                 <meta name="apple-mobile-web-app-capable" content="yes" />
                 <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-
                 <link rel="apple-touch-startup-image" href="./splashscreen.png" />
+
+                <meta name="theme-color" content="#5D4BE1" />
 
                 <link rel="manifest" href="./manifest.json" />
             </head>
